7b5b60aec7eae35bdb7de7152e621d259e8ad304
robby <robby@LAPTOP-KHUOAICR>
2024-02-01 19:20:30 +0100

    Aggiungere i file di progetto.

---
+++ persona/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persona
+{
+    internal class Persona
+    {
+        public string nome { get; set; }
+        public int eta { get; set; }    
+        List<Persona> persone { get; set;}
+    }
+}
---
+++ persona/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            int NumeroPersone=int.Parse(Console.ReadLine());
+            List<Persona> persone = new List<Persona>();
+            for(int i = 0; i < NumeroPersone; i++) 
+            {
+                p.nome = Console.ReadLine();
+                p.eta=int.Parse(Console.ReadLine());
+                if(p.eta > 17)
+                {
+                    persone.Add(p);
+                }
+                 
+            }
+           foreach(Persona persona in persone)
+            {
+                Console.WriteLine("{0},{1} anni", p.nome, p.eta);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
